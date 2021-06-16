05c19ea55a90f0a688ce4654cd4cf8742fde905e
Milos Srejic <srejicmilos@hotmail.com>
2021-06-16 19:10:23 +0200

    Added function for adding new todo

--- src/app.js
+++ src/app.js
@@ -7,4 +7,29 @@ window.onload = function () {
       list.removeChild(li);
     }
   });
+
+  // ADD NEW TODO
+  const addForm = document.forms['add-form'];
+  addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input').value;
+    if (value == '') {
+      alert('Please enter name of new todo');
+    } else {
+      const li = document.createElement('li');
+      const name = document.createElement('span');
+      const deleteBtn = document.createElement('span');
+
+      name.textContent = value;
+      deleteBtn.textContent = 'delete';
+
+      name.classList.add('name');
+      deleteBtn.classList.add('delete');
+
+      li.appendChild(name);
+      li.appendChild(deleteBtn);
+      list.appendChild(li);
+      addForm.querySelector('input').value = '';
+    }
+  });
 };
